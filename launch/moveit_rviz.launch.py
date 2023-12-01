9fffa1e9f6ec642599f69a0674f5d4cf461f574a
David ter Kuile <doterkuile@gmail.com>
2023-12-02 00:17:11 +0100

    update to  new robot argument method

--- launch/moveit_rviz.launch.py
+++ launch/moveit_rviz.launch.py
@@ -21,13 +21,27 @@ from launch_pal.arg_utils import read_launch_argument
 from launch_ros.actions import Node
 
 from moveit_configs_utils import MoveItConfigsBuilder
+from launch_pal.arg_utils import LaunchArgumentsBase
+from launch_pal.robot_arguments import TiagoDualArgs
 from tiago_dual_description.tiago_dual_launch_utils import get_tiago_dual_hw_suffix
-from launch_pal.arg_utils import LaunchArgumentsBase, launch_arg_factory
+from launch_pal.arg_utils import LaunchArgumentsBase
 from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class ArgumentDeclaration(LaunchArgumentsBase):
+class LaunchArguments(LaunchArgumentsBase):
+    base_type: DeclareLaunchArgument = TiagoDualArgs.base_type
+    arm_type_right: DeclareLaunchArgument = TiagoDualArgs.arm_type_right
+    arm_type_left: DeclareLaunchArgument = TiagoDualArgs.arm_type_left
+    end_effector_right: DeclareLaunchArgument = TiagoDualArgs.end_effector_right
+    end_effector_left: DeclareLaunchArgument = TiagoDualArgs.end_effector_left
+    ft_sensor_right: DeclareLaunchArgument = TiagoDualArgs.ft_sensor_right
+    ft_sensor_left: DeclareLaunchArgument = TiagoDualArgs.ft_sensor_left
+    wrist_model_right: DeclareLaunchArgument = TiagoDualArgs.wrist_model_right
+    wrist_model_left: DeclareLaunchArgument = TiagoDualArgs.wrist_model_left
+    camera_model: DeclareLaunchArgument = TiagoDualArgs.camera_model
+    laser_model: DeclareLaunchArgument = TiagoDualArgs.laser_model
+
     use_sim_time: DeclareLaunchArgument = DeclareLaunchArgument(
         name='use_sim_time',
         default_value='False',
@@ -38,20 +52,16 @@ def generate_launch_description():
 
     # Create the launch description and populate
     ld = LaunchDescription()
-    robot_name = "tiago_dual"
-    has_robot_config = True
-    custom_args = ArgumentDeclaration()
-    launch_args = launch_arg_factory(custom_args,
-                                     has_robot_config=has_robot_config, robot_name=robot_name)
+    launch_arguments = LaunchArguments()
 
-    launch_args.add_to_launch_description(ld)
+    launch_arguments.add_to_launch_description(ld)
 
-    declare_actions(ld, launch_args)
+    declare_actions(ld, launch_arguments)
 
     return ld
 
 
-def declare_actions(launch_description: LaunchDescription, launch_args: LaunchArgumentsBase):
+def declare_actions(launch_description: LaunchDescription, launch_args: LaunchArguments):
 
     launch_description.add_action(OpaqueFunction(function=start_rviz))
     return
