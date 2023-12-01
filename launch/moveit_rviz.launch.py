b6d0ea5cfcd8b9045a79991ff65ded1471443eeb
David ter Kuile <david.terkuile@pal-robotics.com>
2023-12-01 16:13:48 +0100

    Add license and contributing

--- launch/moveit_rviz.launch.py
+++ launch/moveit_rviz.launch.py
@@ -17,7 +17,6 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import LaunchConfiguration
 from launch_pal.arg_utils import read_launch_argument
 from launch_ros.actions import Node
 
